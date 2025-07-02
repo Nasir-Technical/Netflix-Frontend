b0a073af953241b908ca3bd31553f9cd524b7525
Nasir-Technical <nasir.18.2006@gmail.com>
2025-07-02 18:53:13 +0500

    changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 // export const API_END_POINT = "http://localhost:8080/api/v1/user";
-
-export const API_END_POINT = "http://16.170.203.78:8080/api/v1/user";
+export const API_END_POINT = "https://stockholm-plate-kate-civil.trycloudflare.com/api/v1/user";
+// export const API_END_POINT = "http://16.170.203.78:8080/api/v1/user";
 
 export const options = {
     method: 'GET',

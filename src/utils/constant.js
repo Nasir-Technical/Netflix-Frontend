dca559417347cafbafbc5e77b69aaa4f01aa9030
Nasir-Technical <nasir.18.2006@gmail.com>
2025-11-26 15:42:49 +0500

    bug fixing frontend

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
-// export const API_END_POINT = "http://localhost:8080/api/v1/user";
+export const API_END_POINT = "http://localhost:8080/api/v1/user";
 
-export const API_END_POINT = "https://netflix-backend-production-9f31.up.railway.app/api/v1/user";
+// export const API_END_POINT = "https://netflix-backend-production-9f31.up.railway.app/api/v1/user";
 
 export const options = {
     method: 'GET',
@@ -14,6 +14,6 @@ export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
 export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
 export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
 
-export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
+export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query=";
 
 export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";

d93995d2b6c229c66c8c492982cb6ef703534e99
Nasir-Technical <nasir.18.2006@gmail.com>
2026-01-12 21:08:31 +0500

    url update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
-export const API_END_POINT = "http://localhost:8080/api/v1/user";
+// export const API_END_POINT = "http://localhost:8080/api/v1/user";
 
-// export const API_END_POINT = "https://netflix-backend-tawny.vercel.app/api/v1/user";
+export const API_END_POINT = "https://netflix-backend-tawny.vercel.app/api/v1/user";
 
 export const options = {
     method: 'GET',
